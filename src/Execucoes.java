d7c106bcb305141fac9d5d71d828f17950c8539b
Ana de Alexandria <anapaula.jp.br@gmail.com>
2020-07-01 22:13:42 -0300

    Posicao

---
+++ src/Execucoes.java
@@ -0,0 +1,12 @@
+import CamadaDeTabuleiro.Posicao;
+
+public class Execucoes {
+
+	public static void main(String[] args) {
+		
+		Posicao posicao = new Posicao(9, 8);
+		System.out.println(posicao.toString());
+
+	}
+
+}
