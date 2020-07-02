80e66cd8df8a7970095277e5bf3b4cf47770cb43
Ana de Alexandria <anapaula.jp.br@gmail.com>
2020-07-01 22:36:55 -0300

    Tabuleiro e Peca

---
+++ src/CamadaDeTabuleiro/Tabuleiro.java
@@ -0,0 +1,36 @@
+package CamadaDeTabuleiro;
+
+public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;	
+	private Peca pecas [][];
+	
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		pecas = new Peca[linhas][colunas];
+	}
+	
+//	public Peca peca(int linhas, int colunas) {
+//		return
+//	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	
+}
--- src/Execucoes.java
+++ src/Execucoes.java
@@ -1,11 +1,10 @@
-import CamadaDeTabuleiro.Posicao;
+import CamadaDeTabuleiro.Tabuleiro;
 
 public class Execucoes {
 
 	public static void main(String[] args) {
-		
-		Posicao posicao = new Posicao(9, 8);
-		System.out.println(posicao.toString());
+
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
 	}
 
